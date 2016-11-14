f1a5ddb49a6e546feeb3b054ea7d75c4b2b7028b
jeff chiu <zhe.chiu@gmail.com>
2016-11-14 11:27:54 +0800

    1st commit

---
+++ FinalAssessment3_Post/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  FinalAssessment3_Post
+//
+//  Created by Chiu Chih-Che on 2016/11/11.
+//  Copyright © 2016年 Jeff. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        var urlString: String = "https://httpbin.org/get"
+        Alamofire.request(.GET, urlString, parameters: nil).responseJSON(){
+                response in
+                if let jsonData = response.result.value {
+                    print("response : \(jsonData)")
+                    let json = JSON(jsonData)
+                    let originValue = json["origin"].stringValue
+                    print("origin value = \(originValue)")
+                }
+        }
+        
+        var nowObj = NSDate()
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let nowStr = dateFormatter.stringFromDate(nowObj)
+        let timestampStart = nowObj.timeIntervalSince1970
+        
+        urlString = "https://httpbin.org/post"
+        Alamofire.request(.POST, urlString, parameters: [
+            "time" : nowStr
+            ]).responseJSON(){
+            response in
+            if let jsonData = response.result.value {
+                print("response : \(jsonData)")
+                nowObj = NSDate()
+                let timestampEnd = nowObj.timeIntervalSince1970
+                print("請求回應時間： \(timestampEnd - timestampStart) 秒")
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'FinalAssessment3_Post' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for FinalAssessment3_Post
+  pod 'Alamofire', '~> 3.0'
+  pod 'SwiftyJSON', '~> 2.4'
+
+end
